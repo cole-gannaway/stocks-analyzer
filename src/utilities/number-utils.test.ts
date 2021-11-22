e8c8f2fce2866e78724fa36c842bbe1dd04f3184
Chad Hollman <hollmanchad@gmail.com>
2021-11-21 19:10:51 -0500

    test: cover new functions

--- src/utilities/number-utils.test.ts
+++ src/utilities/number-utils.test.ts
@@ -1,4 +1,5 @@
-import { formatWithCommas, roundDecimalPlaces } from "./number-utils";
+import { execPath } from "process";
+import { formatWithCommas, roundDecimalPlaces, toFixedUnlessZero } from "./number-utils";
 
 describe('number utils', () => {
     describe('format with commas', () => {
@@ -12,6 +13,16 @@ describe('number utils', () => {
             // assert
             expect(actual).toBe(expected);
         });
+
+        it('should not apply comma separation to numbers less than 1000', () => {
+            const formatter = new Intl.NumberFormat('en-US');
+            const val = 999;
+            const expected = '999';
+
+            const actual = formatWithCommas(formatter, val);
+
+            expect(actual).toBe(expected);
+        })
     });
 
     describe('round decimal places', () => {
@@ -21,6 +32,40 @@ describe('number utils', () => {
             const expected = 1.23;
 
             const actual = roundDecimalPlaces(val, decimalPlaces);
+
+            expect(actual).toBe(expected);
+        });
+    });
+
+    describe('to fixed unless zero', () => {
+        it('should return a value with 2 digit precision', () => {
+            const val = 1.236;
+            const fractionDigits = 2;
+
+            const expected = '1.24';
+
+            const actual = toFixedUnlessZero(val, fractionDigits);
+
+            expect(actual).toBe(expected);
+        });
+
+        it('should return a value with 4 digit precision', () => {
+            const val = 1.32548;
+            const fractionDigits = 4;
+            const expected = '1.3255';
+
+            const actual = toFixedUnlessZero(val, fractionDigits);
+
+            expect(actual).toBe(expected);
+        });
+
+        it('should return a 0.00 string if passed 0', () => {
+            const val = 0;
+            const fractionDigits = 2;
+
+            const expected = '0';
+
+            const actual = toFixedUnlessZero(val, fractionDigits);
 
             expect(actual).toBe(expected);
         });
