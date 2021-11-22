ff92464537988af99c854757fa66f2478357822b
Chad Hollman <hollmanchad@gmail.com>
2021-11-21 20:10:43 -0500

    refactor: code golf date utils

--- src/utilities/date-utils.ts
+++ src/utilities/date-utils.ts
@@ -2,24 +2,13 @@ export const MILLIS_PER_WEEK = 1000 * 60 * 60 * 24 * 7;
 
 export function getMonthInMMFormat(month: number) {
   const actualMonth = month + 1;
-  if (actualMonth < 10) {
-    return '0' + actualMonth.toString();
-  } else {
-    return actualMonth.toString();
-  }
+  return actualMonth < 10 ? `0${actualMonth}` : `${actualMonth}`;
 }
 export function getDateinDDFormat(date: number) {
-  if (date < 10) {
-    return '0' + date.toString();
-  } else {
-    return date.toString();
-  }
+  return date < 10 ? `0${date}` : `${date}`;
 }
 export function compareDates(a: Date, b: Date) {
   let compare = a.getFullYear() - b.getFullYear();
-  if (compare === 0) {
-    compare = a.getMonth() - b.getMonth();
-  }
   if (compare === 0) {
     compare = a.getDate() - b.getDate();
   }
