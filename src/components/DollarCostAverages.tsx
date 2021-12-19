135d355b59abaf0403eaecbc51fe9b1ec6ea79d5
Cole Gannaway <43042842+cole-gannaway@users.noreply.github.com>
2021-12-19 13:52:50 -0600

    Updated dashboard to be cleaner

--- src/components/DollarCostAverages.tsx
+++ src/components/DollarCostAverages.tsx
@@ -1,4 +1,3 @@
-import { Grid } from "@material-ui/core";
 import { useAppSelector } from "../app/hooks";
 import { selectCurrentPrices } from "../slices/currentPricesSlice";
 import { selectDCAProfitSummary, selectDCASummaries } from "../slices/transactionsSlice";
@@ -9,79 +8,52 @@ export function DollarCostAverages() {
     const dcaProfitSummary = useAppSelector(selectDCAProfitSummary);
     const currentPrices = useAppSelector(selectCurrentPrices);
 
-    const symbols = Object.keys(dcaSummaries);
-    const symbolsAndPrices = symbols.filter(symbol => currentPrices[symbol]).map(symbol => {
-        return { symbol: symbol, price: currentPrices[symbol] };
+    let currentValueSum = 0;
+    let unrealizedProfitsSum = 0;
+    let allTimeProfitsSum = 0;
+    const dashboard = Object.entries(dcaProfitSummary.profits).map((entry) => {
+        const symbol = entry[0];
+        const profits = entry[1];
+        const amount: number = dcaProfitSummary.notSoldAmount[symbol] ? dcaProfitSummary.notSoldAmount[symbol] : 0;
+        const currentPrice: number | undefined = currentPrices[symbol];
+        const currentValue = (currentPrice && amount) ? currentPrice * amount : 0;
+        const dca = dcaSummaries[symbol] ? dcaSummaries[symbol].price : 0;
+        const totalCost = dca * amount;
+        const potentialEarnings = currentPrice * amount;
+        const unrealizedProfits = potentialEarnings - totalCost;
+
+        // update running totals
+        currentValueSum+= currentValue;
+        unrealizedProfitsSum += unrealizedProfits;
+        allTimeProfitsSum += profits;
+
+        return <tr key={"dashboard-" + symbol}>
+            <td>{symbol}</td>
+            <td>{currentValue !== 0 ? "$" + toFixedUnlessZero(currentValue, 2) : "-"}</td>
+            <td>{amount ? amount.toFixed(2) : "-"}</td>
+            <td>{currentPrice ? "$" + toFixedUnlessZero(currentPrice, 2) : "-"}</td>
+            <td>{dca !== 0 ? "$" + toFixedUnlessZero(dca, 2) : "-"}</td>
+            <td>{dca !== 0 ? toFixedUnlessZero(((currentPrice - dca) / dca * 100), 2) + "%" : "-"}</td>
+            <td>{unrealizedProfits !== 0 ? "$" + toFixedUnlessZero(unrealizedProfits, 2) : "-"}</td>
+            <td>{profits !== 0 ? "$" + toFixedUnlessZero(profits, 2) : "-"}</td>
+        </tr>
     });
 
-    return <div>
-        <h3>Wallet</h3>
-        <table key="wallet">
-            <tbody>
-                {Object.entries(dcaProfitSummary.notSoldAmount).map((entry) => {
-                    const symbol = entry[0];
-                    const amount = entry[1];
-                    return <tr key={"wallet-" + symbol}><td>{symbol}</td><td>{amount.toFixed(2)}</td><td></td></tr>
-                })}
-            </tbody>
+    return <div style={{textAlign: "center"}}>
+        <h1>Dashboard</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Symbol</th><th>Current Value</th><th>Amount</th><th>Current Price</th><th>Dollar Cost Average</th><th>% Return</th><th>Unrealized Profits</th><th>All Time Profits</th>
+                </tr>
+            </thead>
+            {dashboard}
+            <tfoot>
+                <tr>
+                <td>Total</td><td>{"$" + toFixedUnlessZero(currentValueSum,2)}</td><td></td><td></td><td></td><td></td><td>{"$" + toFixedUnlessZero(unrealizedProfitsSum,2)}</td><td>{"$" + toFixedUnlessZero(allTimeProfitsSum,2)}</td>
+                </tr>
+            </tfoot>
+
         </table>
-        <Grid container spacing={3}>
-            <Grid item xs={12} sm={6}>
-                <h3>Dollar Cost Averages</h3>
-                <table key={"dca-summary"}>
-                    <tbody>
-                        {Object.entries(dcaSummaries).map((entry) => {
-                            const symbol = entry[0];
-                            const summary = entry[1];
-                            const dca = summary.price;
-                            return <tr key={"dca-summary-" + symbol}><td>{symbol}</td><td>${toFixedUnlessZero(dca, 2)}</td></tr>
-                        })}
-                    </tbody>
-                </table>
-
-            </Grid>
-            <Grid item xs={12} sm={6}>
-                <h3>Current Prices</h3>
-                <table key={"current-prices"}>
-                    <tbody>
-                        {symbolsAndPrices.map(data => {
-                            return <tr key={"current-prices-" + data.symbol}><td>{data.symbol}</td><td>${toFixedUnlessZero(data.price, 2)}</td></tr>
-                        })}
-                    </tbody>
-                </table>
-
-            </Grid>
-            <Grid item xs={12} sm={6}>
-                <h3>Unrealized Profits</h3>
-                <table key="dca-unrealized-profits">
-                    <tbody>
-                        {Object.entries(dcaProfitSummary.notSoldAmount).map((entry) => {
-                            const symbol = entry[0];
-                            const amount = entry[1];
-                            const dcaPrice = dcaSummaries[symbol].price;
-                            const totalCost = dcaPrice * amount;
-                            const currentPrice = currentPrices[symbol];
-                            const potentialEarnings = currentPrice * amount;
-                            const unrealizedProfits = potentialEarnings - totalCost;
-                            return <tr key={"dca-unrealized-profits-" + symbol}><td>{symbol}</td><td>${unrealizedProfits.toFixed(2)}</td><td>{toFixedUnlessZero(((currentPrice - dcaPrice) / dcaPrice * 100), 2) + "%"}</td></tr>
-                        })}
-                    </tbody>
-                </table>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-                <h3>Profits</h3>
-                <table key="dca-profits">
-                    <tbody>
-                        {Object.entries(dcaProfitSummary.profits).map((entry) => {
-                            const symbol = entry[0];
-                            const profits = entry[1];
-                            return <tr key={"dca-profits-" + symbol}><td>{symbol}</td><td>${profits.toFixed(2)}</td></tr>
-                        })}
-                        <tr key={"dca-profits-sum"}><td>Total</td><td>${Object.values(dcaProfitSummary.profits).reduce((prev, curr) => prev + curr, 0).toFixed(2)}</td></tr>
-                    </tbody>
-                </table>
-            </Grid>
-        </Grid>
-
     </div>
 }
